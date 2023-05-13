241518049d12be7e6fc9ec63c1905536cb1fabbf
krzysztofskul <krzysztofskul@protonmail.com>
2023-05-13 22:52:15 +0200

    Jar file created. Main method added in App class.

--- src/main/java/pl/krzysztofskul/sensit5/App.java
+++ src/main/java/pl/krzysztofskul/sensit5/App.java
@@ -11,10 +11,10 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
  */
 public class App extends AbstractAnnotationConfigDispatcherServletInitializer {
 	
-//    public static void main( String[] args )
-//    {
-//        System.out.println( "Hello World!" );
-//    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World sensit5!" );
+    }
     
     @Override
     protected Class<?>[] getRootConfigClasses() { return null; }
